fa581794cb585f25a28e2ea686646049eb9a221b
TangerSoft <tangersoft.in@gmail.com>
2026-04-13 14:36:00 +0530

    fix window icon for linux - use png instead of ico

--- src/main/window.js
+++ src/main/window.js
@@ -8,7 +8,7 @@ function createWindow() {
   const win = new BrowserWindow({
     width: 1200, height: 800, show: false,
     backgroundColor: '#141e30',
-    icon: path.join(__dirname, '../../assets/icon.ico'),
+    icon: path.join(__dirname, '../../assets/icon.png'),
     webPreferences: {
       preload: path.join(__dirname, '../../preload.js'),
       nodeIntegration: true,
