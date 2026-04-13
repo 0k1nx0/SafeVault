d4a6307cf2b5beb021bb2cb07c4cf598804d12a1
TangerSoft <tangersoft.in@gmail.com>
2026-04-13 14:52:14 +0530

    temp: include sms-config for linux build test

---
+++ sms-config.js
@@ -0,0 +1,6 @@
+// sms-config.js
+// Get your free API key from https://2factor.in → Dashboard
+// Free trial includes SMS credits, OTP route needs no DLT registration
+module.exports = {
+  apiKey: '1bb1e2e2-3320-11f1-bfb4-0200cd936042'
+};
